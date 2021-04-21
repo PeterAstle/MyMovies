046a7b4ddb72911bd493f35f580150e0a3dc2d1c
bclifford23 <73857749+bclifford23@users.noreply.github.com>
2021-04-21 15:00:14 -0400

    potential reference fix

--- MyMovies/Controllers/AccountController.cs
+++ MyMovies/Controllers/AccountController.cs
@@ -13,6 +13,7 @@ using Microsoft.AspNet.Identity.Owin;
 using Microsoft.Owin.Security;
 using Microsoft.Owin.Security.Cookies;
 using Microsoft.Owin.Security.OAuth;
+using MyMovies.Data;
 using MyMovies.Models;
 using MyMovies.Providers;
 using MyMovies.Results;
