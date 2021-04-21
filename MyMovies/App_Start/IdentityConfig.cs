b1b86a48a73ffd5a54058aba398958415388b92d
bclifford23 <73857749+bclifford23@users.noreply.github.com>
2021-04-21 14:53:10 -0400

    ticket 18, add props to ratings, add props to rating models, add createrating method in services, add identity models config

--- MyMovies/App_Start/IdentityConfig.cs
+++ MyMovies/App_Start/IdentityConfig.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNet.Identity;
 using Microsoft.AspNet.Identity.EntityFramework;
 using Microsoft.AspNet.Identity.Owin;
 using Microsoft.Owin;
+using MyMovies.Data;
 using MyMovies.Models;
 
 namespace MyMovies
