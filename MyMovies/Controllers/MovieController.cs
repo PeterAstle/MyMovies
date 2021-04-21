76a81a26bc538139c62816a26fd516d0a42d1541
PeterAstle <79322373+PeterAstle@users.noreply.github.com>
2021-04-21 16:16:47 -0400

    ticket 27 initial commit, movie POST functionality.

--- MyMovies/Controllers/MovieController.cs
+++ MyMovies/Controllers/MovieController.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using Microsoft.AspNet.Identity;
+using MyMovie.Services;
+using MyMovies.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net;
@@ -7,7 +10,33 @@ using System.Web.Http;
 
 namespace MyMovies.Controllers
 {
+    [Authorize]
     public class MovieController : ApiController
     {
+        private MovieService CreateMovieService()
+        {
+            var userId = Guid.Parse(User.Identity.GetUserId());
+
+            var movieService = new MovieService(userId);
+
+            return movieService;
+        }
+
+        public IHttpActionResult Post(MovieCreate movie)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var service = CreateMovieService();
+
+            if (!service.CreateMovie(movie))
+            {
+                return InternalServerError();
+            }
+
+            return Ok();
+        }
     }
 }
