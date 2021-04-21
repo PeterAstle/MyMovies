a33aa0e2acadaeeff9874df858d765bd574a86bd
bclifford23 <73857749+bclifford23@users.noreply.github.com>
2021-04-21 12:15:21 -0400

    fix push

--- MyMovies.Models/RatingModels/RatingCreate.cs
+++ MyMovies.Models/RatingModels/RatingCreate.cs
@@ -1,10 +1,10 @@
-﻿using MyMovies.Data;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using MyMovies.Data;
 
 namespace MyMovies.Models.RatingModels
 {
