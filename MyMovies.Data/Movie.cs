05588fdbfaf5575928da71b07d8455b9190f6e1b
PeterAstle <79322373+PeterAstle@users.noreply.github.com>
2021-04-21 11:05:06 -0400

    ticket 2 initial push

--- MyMovies.Data/Movie.cs
+++ MyMovies.Data/Movie.cs
@@ -1,13 +1,64 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace MyMovies.Data
 {
+        public enum MovieGenre
+        {
+            Horror, RomCom, SciFi, Documentary, Action, Comedy, Drama
+        }
+        public enum MaturityRating
+        {
+            G, PG, PG13, R, NC17
+        }
+
     public class Movie
     {
 
+        [Key]
+        public int MovieId { get; set; }
+        [Required]
+        public string MovieTitle { get; set; }
+        [Required]
+        public string MovieDescription { get; set; }
+
+       
+        public virtual List<Rating> Ratings { get; set; } = new List<Rating>();
+
+        [Required]
+        public double MovieRating { get; set; }
+
+        [Required]
+        public MovieGenre Genre { get; set; }
+
+        public string MovieNote { get; set; }
+
+        [Required]
+        public bool WouldWatchAgain { get; set; }
+
+        [Required]
+
+        public MaturityRating RatingForMaturity { get; set; }
+
+        public virtual List<Favorite> FavList { get; set; }
+
+        [Required]
+        public bool IsFavorite { get; set; }
+
+        public Guid OwnerId { get; set; }
+
+        [Required]
+        public DateTimeOffset CreatedUtc { get; set; }
+
+        public DateTimeOffset ModifiedUtc { get; set; }
+
+
+
+
     }
+
 }
