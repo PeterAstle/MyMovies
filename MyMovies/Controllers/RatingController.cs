7d56435156c07780d4493abaa4b037623474d408
bclifford23 <73857749+bclifford23@users.noreply.github.com>
2021-04-22 10:19:15 -0400

    adding  get controller method

--- MyMovies/Controllers/RatingController.cs
+++ MyMovies/Controllers/RatingController.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Microsoft.AspNet.Identity;
+using MyMovie.Services;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net;
@@ -9,5 +11,24 @@ namespace MyMovies.Controllers
 {
     public class RatingController : ApiController
     {
+        public IHttpActionResult Get()
+        {
+            RatingService ratingService = CreateRatingService();
+            var ratings = ratingService.GetRatings();
+            return Ok(ratings);
+        }
+        public IHttpActionResult Get(int id)
+        {
+            RatingService ratingService = CreateRatingService();
+            var rating = ratingService.GetRatingById(id);
+            return Ok(rating);
+        }
+        private RatingService CreateRatingService()
+        {
+            var userId = Guid.Parse(User.Identity.GetUserId());
+            var ratingService = new RatingService(userId);
+            return ratingService;
+        }
     }
+    
 }
